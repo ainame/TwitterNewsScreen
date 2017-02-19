a503ebf13a0f4ed502c0dd57e0708bc0f6e4c71a
ainame <s.namai.09@gmail.com>
2017-02-19 23:05:02 +0900

    Add scroll timer

--- TwitterNewsScreen/Scenes/ImageScreen/ImageScreenViewController.swift
+++ TwitterNewsScreen/Scenes/ImageScreen/ImageScreenViewController.swift
@@ -13,14 +13,31 @@ class ImageScreenViewController: UIViewController {
     @IBOutlet weak var collectionView: UICollectionView!
     let viewModel = TweetSearcherViewModel()
     let disposeBag = DisposeBag()
+    var timer: Timer?
+    var currentIndexPath: IndexPath? {
+        return self.collectionView.indexPathsForVisibleItems.first
+    }
     var tweets = [Tweet]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         setupLayout()
 
-        viewModel.show(forId: "830745950922551296")
+        timer = Timer.scheduledTimer(withTimeInterval: 3, repeats: true) { _ in
+            print("page! \(self.currentIndexPath)")
+            guard let currentIndexPath = self.currentIndexPath else { return }
+
+            let max = self.collectionView.numberOfItems(inSection: currentIndexPath.section)
+            if max > 0 && currentIndexPath.item < max - 1 {
+                let nextIndexPath = IndexPath(item: currentIndexPath.item + 1, section: currentIndexPath.section)
+                self.collectionView.scrollToItem(at: nextIndexPath, at: .centeredHorizontally, animated: true)
+            }
+        }
+
+        viewModel
+            .show(forId: "830745950922551296")
             .map { tweet in [tweet, tweet, tweet, tweet, tweet, tweet, tweet] }
+            .do(onNext: { _ in self.timer?.fire() })
             .bindTo(
                 collectionView.rx.items(cellIdentifier: ImageScreenCell.identifier, cellType: ImageScreenCell.self)
             ) { (_, element, cell) in
